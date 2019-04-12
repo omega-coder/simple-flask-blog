71007d1c1d948d4316a7bc377c0423436820d7d6
omega-coder <fy_cherief@esi.dz>
2019-04-12 03:19:31 +0200

    Adds TextArea to imports

--- flask_blog/forms.py
+++ flask_blog/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flask_blog.models import User
 from flask_login import current_user
@@ -61,3 +61,15 @@ class UpdateAccountForm(FlaskForm):
 
             if user:
                 raise ValidationError('Email already exists, choose another one')
+
+
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[
+        DataRequired()
+    ])
+
+    content = TextAreaField('Content', validators=[
+        DataRequired()
+    ])
+
+    submit = SubmitField('Post')
